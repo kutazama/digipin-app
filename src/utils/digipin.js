03bbb3fff0512dbc6137ecd3b7d3304e45251d1f
Devdatta Tengshe <devdatta@tengshe.in>
2024-07-25 20:58:57 +0530

    fix: Generating the Right digipin code

--- src/utils/digipin.js
+++ src/utils/digipin.js
@@ -16,18 +16,45 @@ export function Get_DIGIPIN(lat, lon) {
     var vDIGIPIN = ''; 
     var MinLat = 1.50; var MaxLat = 39.00; var MinLon = 63.50; var MaxLon = 99.00;
     var LatDivBy = 4; var LonDivBy = 4;
+    var LatDivDeg = 0; var LonDivDeg = 0;
+    var r = 0; var c = 0; 
+    var LatDivDeg = 0; var LonDivDeg = 0;
 
     if (lat < MinLat || lat > MaxLat || lon < MinLon || lon > MaxLon) {
         return 'Out of Bound';
     }
 
     for (let Lvl = 1; Lvl <= 10; Lvl++) {
-        var LatDivDeg = (MaxLat - MinLat) / LatDivBy;
-        var LonDivDeg = (MaxLon - MinLon) / LonDivBy;
+        LatDivDeg = (MaxLat - MinLat) / LatDivBy;
+        LonDivDeg = (MaxLon - MinLon) / LonDivBy;
 
-        var r = Math.floor((lat - MinLat) / LatDivDeg);
-        var c = Math.floor((lon - MinLon) / LonDivDeg);
+        var NextLvlMaxLat = MaxLat;
+        var NextLvlMinLat = MaxLat - LatDivDeg;
 
+        for (var x = 0; x < LatDivBy; x++) {
+            if (lat >= NextLvlMinLat && lat < NextLvlMaxLat) {
+                r = x;
+                break;
+            }
+            else {
+                NextLvlMaxLat = NextLvlMinLat
+                NextLvlMinLat = NextLvlMaxLat - LatDivDeg;
+            }
+        }
+
+        var NextLvlMinLon = MinLon;
+        var NextLvlMaxLon = MinLon + LonDivDeg;
+
+        for (var x = 0; x < LonDivBy; x++) {
+            if (lon >= NextLvlMinLon && lon < NextLvlMaxLon) {
+                c = x;
+                break;
+            }
+            else {
+                NextLvlMinLon = NextLvlMaxLon;
+                NextLvlMaxLon = NextLvlMinLon + LonDivDeg;
+            }
+        }
         if (Lvl === 1) {
             if (L1[r][c] === "0") {
                 return "Out of Bound";
@@ -40,10 +67,8 @@ export function Get_DIGIPIN(lat, lon) {
             }
         }
 
-        MinLat += r * LatDivDeg;
-        MaxLat = MinLat + LatDivDeg;
-        MinLon += c * LonDivDeg;
-        MaxLon = MinLon + LonDivDeg;
+        MinLat = NextLvlMinLat; MaxLat = NextLvlMaxLat;
+        MinLon = NextLvlMinLon; MaxLon = NextLvlMaxLon;
     } 
     return vDIGIPIN;
 }
