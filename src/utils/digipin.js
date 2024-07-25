328e14e56fc28e34c9821ef4a600f8f10639f1cb
Ronit Jadhav <ronitjadhav@wstud2-f25857cde28d.hof-university.de>
2024-07-25 19:30:26 +0200

    Updated the function to get the lat long from digipin

--- src/utils/digipin.js
+++ src/utils/digipin.js
@@ -74,57 +74,70 @@ export function Get_DIGIPIN(lat, lon) {
 }
 
 export function Get_LatLong(digipin) {
-    var L1 = [
+    const L1 = [
         ['0', '2', '0', '0'],
         ['3', '4', '5', '6'],
         ['G', '8', '7', 'M'],
         ['J', '9', 'K', 'L']
     ];
 
-    var L2 = [
+    const L2 = [
         ['J', 'G', '9', '8'],
         ['K', '3', '2', '7'],
         ['L', '4', '5', '6'],
         ['M', 'P', 'W', 'X']
     ];
 
-    var MinLat = 1.50, MaxLat = 39.00, MinLon = 63.50, MaxLon = 99.00;
-    var LatDivBy = 4, LonDivBy = 4;
+    let MinLat = 1.50; let MaxLat = 39.00; let MinLon = 63.50; let MaxLon = 99.00;
+    let LatDivBy = 4; let LonDivBy = 4;
 
-    if (!/^[0-9A-Z-]+$/.test(digipin)) {
+    digipin = digipin.replace(/-/g, '');
+
+    if (digipin.length !== 10) {
         return 'Invalid DIGIPIN';
     }
 
-    for (let Lvl = 1, i = 0; Lvl <= 10; Lvl++, i++) {
-        if (digipin[i] === '-') {
-            i++;
-        }
+    for (let Lvl = 1; Lvl <= 10; Lvl++) {
+        let char = digipin[Lvl - 1];
+        let row, col;
 
-        var LatDivDeg = (MaxLat - MinLat) / LatDivBy;
-        var LonDivDeg = (MaxLon - MinLon) / LonDivBy;
-
-        var found = false;
-        for (let r = 0; r < LatDivBy; r++) {
-            for (let c = 0; c < LonDivBy; c++) {
-                if ((Lvl === 1 && L1[r][c] === digipin[i]) || (Lvl > 1 && L2[r][c] === digipin[i])) {
-                    MinLat += r * LatDivDeg;
-                    MaxLat = MinLat + LatDivDeg;
-                    MinLon += c * LonDivDeg;
-                    MaxLon = MinLon + LonDivDeg;
-                    found = true;
-                    break;
+        if (Lvl === 1) {
+            for (let i = 0; i < 4; i++) {
+                for (let j = 0; j < 4; j++) {
+                    if (L1[i][j] === char) {
+                        row = i;
+                        col = j;
+                        break;
+                    }
                 }
+                if (row !== undefined) break;
             }
-            if (found) break;
+            if (row === undefined) return 'Invalid DIGIPIN';
+        } else {
+            for (let i = 0; i < 4; i++) {
+                for (let j = 0; j < 4; j++) {
+                    if (L2[i][j] === char) {
+                        row = i;
+                        col = j;
+                        break;
+                    }
+                }
+                if (row !== undefined) break;
+            }
+            if (row === undefined) return 'Invalid DIGIPIN';
         }
 
-        if (!found) {
-            return 'Invalid DIGIPIN';
-        }
+        let LatDivDeg = (MaxLat - MinLat) / LatDivBy;
+        let LonDivDeg = (MaxLon - MinLon) / LonDivBy;
+
+        MaxLat = MaxLat - (row * LatDivDeg);
+        MinLat = MaxLat - LatDivDeg;
+        MinLon = MinLon + (col * LonDivDeg);
+        MaxLon = MinLon + LonDivDeg;
     }
 
-    var latitude = (MinLat + MaxLat) / 2;
-    var longitude = (MinLon + MaxLon) / 2;
+    let lat = (MinLat + MaxLat) / 2;
+    let lon = (MinLon + MaxLon) / 2;
 
-    return { latitude, longitude };
+    return { latitude: lat, longitude: lon };
 }
