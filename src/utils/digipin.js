f3519efbd7039360113e48cee02c9942c05df965
GitButler <gitbutler@gitbutler.com>
2024-07-25 14:45:23 +0200

    feat: Add Get_LatLong function and use it in DigiPinMap for searching by DIGIPIN

--- src/utils/digipin.js
+++ src/utils/digipin.js
@@ -72,3 +72,59 @@ export function Get_DIGIPIN(lat, lon) {
     } 
     return vDIGIPIN;
 }
+
+export function Get_LatLong(digipin) {
+    var L1 = [
+        ['0', '2', '0', '0'],
+        ['3', '4', '5', '6'],
+        ['G', '8', '7', 'M'],
+        ['J', '9', 'K', 'L']
+    ];
+
+    var L2 = [
+        ['J', 'G', '9', '8'],
+        ['K', '3', '2', '7'],
+        ['L', '4', '5', '6'],
+        ['M', 'P', 'W', 'X']
+    ];
+
+    var MinLat = 1.50, MaxLat = 39.00, MinLon = 63.50, MaxLon = 99.00;
+    var LatDivBy = 4, LonDivBy = 4;
+
+    if (!/^[0-9A-Z-]+$/.test(digipin)) {
+        return 'Invalid DIGIPIN';
+    }
+
+    for (let Lvl = 1, i = 0; Lvl <= 10; Lvl++, i++) {
+        if (digipin[i] === '-') {
+            i++;
+        }
+
+        var LatDivDeg = (MaxLat - MinLat) / LatDivBy;
+        var LonDivDeg = (MaxLon - MinLon) / LonDivBy;
+
+        var found = false;
+        for (let r = 0; r < LatDivBy; r++) {
+            for (let c = 0; c < LonDivBy; c++) {
+                if ((Lvl === 1 && L1[r][c] === digipin[i]) || (Lvl > 1 && L2[r][c] === digipin[i])) {
+                    MinLat += r * LatDivDeg;
+                    MaxLat = MinLat + LatDivDeg;
+                    MinLon += c * LonDivDeg;
+                    MaxLon = MinLon + LonDivDeg;
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+
+        if (!found) {
+            return 'Invalid DIGIPIN';
+        }
+    }
+
+    var latitude = (MinLat + MaxLat) / 2;
+    var longitude = (MinLon + MaxLon) / 2;
+
+    return { latitude, longitude };
+}
