0dd6111470264175b89d5df062236b96b1e886f3
Devdatta Tengshe <devdatta@tengshe.in>
2024-07-25 21:01:26 +0530

    feat: made code a bit more readable

--- src/utils/digipin.js
+++ src/utils/digipin.js
@@ -1,24 +1,24 @@
 export function Get_DIGIPIN(lat, lon) {
-    var L1 = [
+    const L1 = [
         ['0', '2', '0', '0'],
         ['3', '4', '5', '6'],
         ['G', '8', '7', 'M'],
         ['J', '9', 'K', 'L']
     ];
 
-    var L2 = [
+    const L2 = [
         ['J', 'G', '9', '8'],
         ['K', '3', '2', '7'],
         ['L', '4', '5', '6'],
         ['M', 'P', 'W', 'X']
     ];
 
-    var vDIGIPIN = ''; 
-    var MinLat = 1.50; var MaxLat = 39.00; var MinLon = 63.50; var MaxLon = 99.00;
-    var LatDivBy = 4; var LonDivBy = 4;
-    var LatDivDeg = 0; var LonDivDeg = 0;
-    var r = 0; var c = 0; 
-    var LatDivDeg = 0; var LonDivDeg = 0;
+    let vDIGIPIN = ''; 
+    let MinLat = 1.50; let MaxLat = 39.00; let MinLon = 63.50; let MaxLon = 99.00;
+    let LatDivBy = 4; let LonDivBy = 4;
+    
+    let row = 0; let column = 0; 
+    let LatDivDeg = 0; let LonDivDeg = 0;
 
     if (lat < MinLat || lat > MaxLat || lon < MinLon || lon > MaxLon) {
         return 'Out of Bound';
@@ -28,12 +28,12 @@ export function Get_DIGIPIN(lat, lon) {
         LatDivDeg = (MaxLat - MinLat) / LatDivBy;
         LonDivDeg = (MaxLon - MinLon) / LonDivBy;
 
-        var NextLvlMaxLat = MaxLat;
-        var NextLvlMinLat = MaxLat - LatDivDeg;
+        let NextLvlMaxLat = MaxLat;
+        let NextLvlMinLat = MaxLat - LatDivDeg;
 
-        for (var x = 0; x < LatDivBy; x++) {
+        for (let x = 0; x < LatDivBy; x++) {
             if (lat >= NextLvlMinLat && lat < NextLvlMaxLat) {
-                r = x;
+                row = x;
                 break;
             }
             else {
@@ -42,12 +42,12 @@ export function Get_DIGIPIN(lat, lon) {
             }
         }
 
-        var NextLvlMinLon = MinLon;
-        var NextLvlMaxLon = MinLon + LonDivDeg;
+        let NextLvlMinLon = MinLon;
+        let NextLvlMaxLon = MinLon + LonDivDeg;
 
-        for (var x = 0; x < LonDivBy; x++) {
+        for (let x = 0; x < LonDivBy; x++) {
             if (lon >= NextLvlMinLon && lon < NextLvlMaxLon) {
-                c = x;
+                column = x;
                 break;
             }
             else {
@@ -56,12 +56,12 @@ export function Get_DIGIPIN(lat, lon) {
             }
         }
         if (Lvl === 1) {
-            if (L1[r][c] === "0") {
+            if (L1[row][column] === "0") {
                 return "Out of Bound";
             }
-            vDIGIPIN += L1[r][c];
+            vDIGIPIN += L1[row][column];
         } else {
-            vDIGIPIN += L2[r][c];
+            vDIGIPIN += L2[row][column];
             if (Lvl === 3 || Lvl === 6) {
                 vDIGIPIN += "-";
             }
